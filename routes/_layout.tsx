b73b1fed068eab8066a255b5caa151bb1f7a2bf2
JesusCalleRuiz <146335396+JesusCalleRuiz@users.noreply.github.com>
2024-03-31 22:04:37 +0200

    Update _layout.tsx

--- routes/_layout.tsx
+++ routes/_layout.tsx
@@ -4,9 +4,7 @@ import Menu from "../components/Menu.tsx";
 const Layout = async (req: Request, ctx: FreshContext) => {
   const Component = ctx.Component;
   const route = ctx.route;
-  // last after /
   const page = route.split("/").pop();
-  // if page is empty, it means we are at the root
   const selected = page === "" ? "List" : "Add";
   return (
     <>
